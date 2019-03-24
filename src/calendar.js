184eb04217f3ad7413d6133e7e3727fc26a6e05f
jorgeantrax <spicer_94@hotmail.com>
2019-03-24 08:14:42 -0600

    Added .gitignore and gulp dependence

--- src/calendar.js
+++ src/calendar.js
@@ -1,10 +1,20 @@
-export class Calendar {
-    constructor() {
-        this.buildCalendar('hola calendar');
+export default class Calendar {
+    constructor({ el, language = 'es', name }) {
+        this.el = document.querySelector(el);
+        this.language = language;
+        this.name = name;
+
+        this.build();
     }
 
-    buildCalendar($a) {
-        console.log($a);
+    build() {
+        let $template = `
+					<input type='text' placeholder='01/05/2019'/>
+				`;
+    }
+
+    dev() {
+        console.log(this);
     }
 
 }
