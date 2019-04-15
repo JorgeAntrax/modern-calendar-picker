6cbbd0789ade553cd1d477f9bd5be60cfb1476a7
jorgeantrax <spicer_94@hotmail.com>
2019-04-14 23:43:48 -0600

    configured basic events in calendar

--- build/calendar.js
+++ build/calendar.js
@@ -14,13 +14,15 @@ function () {
         _ref$language = _ref.language,
         language = _ref$language === void 0 ? 'es' : _ref$language,
         name = _ref.name,
-        prefix = _ref.prefix,
+        _ref$prefix = _ref.prefix,
+        prefix = _ref$prefix === void 0 ? 'mcp' : _ref$prefix,
         _ref$date = _ref.date,
         date = _ref$date === void 0 ? null : _ref$date;
 
     _classCallCheck(this, Calendar);
 
     this.el = document.querySelector(el);
+    this.that = el.split('#').join('');
     this.language = language;
     this.name = name;
     this.prefix = prefix;
@@ -28,7 +30,10 @@ function () {
     this.date = new Date();
     this.initDate = date != null || date != undefined ? date.split('/') : null;
     this.calendarGrid = null;
-    this.inputID = "".concat(el.split('#').join(''), "-input");
+    this.iconToggle = "".concat(this.prefix, "-").concat(this.that, "-dateNow");
+    this.iconClear = "".concat(this.prefix, "-").concat(this.that, "-clear");
+    this.inputId = "".concat(this.prefix, "-").concat(this.that, "-input");
+    this.toggleView = "".concat(this.prefix, "-").concat(this.that, "-toggle-view");
 
     if (this.initDate != null && Array.isArray(this.initDate)) {
       this.setDay(this.initDate[0]);
@@ -40,7 +45,7 @@ function () {
       this.setYear(this.date.getFullYear());
     }
 
-    this.build(this.inputID);
+    this.build(this.iconToggle, this.inputId, this.el);
   }
 
   _createClass(Calendar, [{
@@ -52,6 +57,7 @@ function () {
           this.months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
           this.cancelBtn = 'cancel';
           this.saveBtn = 'save';
+          this.todayBtn = 'today';
           break;
 
         default:
@@ -59,32 +65,163 @@ function () {
           this.months = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
           this.cancelBtn = 'cancelar';
           this.saveBtn = 'aceptar';
+          this.todayBtn = 'Hoy';
           break;
       }
     }
   }, {
     key: "build",
-    value: function build(id) {
+    value: function build(id, input, parent) {
       this.setLanguage(this.language);
-      var $template = "\n\t\t\t\t<input type='text' class=\"".concat(this.prefix, "-input\" id=\"").concat(id, "\" placeholder='01/05/2019'/>\n\t\t\t\t<div class=\"").concat(this.prefix, "-calendar\">\n\t\t\t\t\t<div class=\"").concat(this.prefix, "-calendar-header\">\n\t\t\t\t\t\t\t<span class=\"").concat(this.prefix, "-calendar-control previus\"></span>\n\t\t\t\t\t\t\t<strong>{{month}} {{year}}</strong>\n\t\t\t\t\t\t\t<span class=\"").concat(this.prefix, "-calendar-control next\"></span>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class=\"").concat(this.prefix, "-calendar-days\">{{days}}</div>\n\t\t\t\t\t<div class=\"").concat(this.prefix, "-calendar-grid\">{{calendar-grid}}</div>\n\t\t\t\t\t<div class=\"").concat(this.prefix, "-calendar-actions\">\n\t\t\t\t\t\t\t<button cancel>").concat(this.cancelBtn, "</button>\n\t\t\t\t\t\t\t<button save>").concat(this.saveBtn, "</button>\n\t\t\t\t\t</div>\n\t\t\t</div>\n\t\t\t");
-      $template = $template.replace(/\{\{days\}\}/g, this.getDays()).replace(/\{\{month\}\}/g, this.months[this.getMonth() - 1]).replace(/\{\{year\}\}/g, this.getYear()).replace(/\{\{calendar-grid\}\}/g, this.getCalendar()).replace('\t', '').replace('\n', '');
+      var $template = "\n\t\t\t\t<span class=\"".concat(this.prefix, "-icon-clear hide\" id=\"").concat(this.iconClear, "\">\n\t\t\t\t\t<span></span>\n\t\t\t\t</span>\n\t\t\t\t<input maxlength=\"10\" type='text' class=\"").concat(this.prefix, "-input\" id=\"").concat(input, "\" placeholder='01/05/2019'/>\n\t\t\t\t<span id=\"").concat(this.iconToggle, "\" class=\"").concat(this.prefix, "-icon-get-date-now\">\n\t\t\t\t\t<span></span>\n\t\t\t\t</span>\n\t\t\t\t<div class=\"").concat(this.prefix, "-calendar\">\n\t\t\t\t\t<div class=\"").concat(this.prefix, "-calendar-header\">\n\t\t\t\t\t\t\t<span class=\"").concat(this.prefix, "-calendar-control previus\"></span>\n\t\t\t\t\t\t\t<strong id=\"").concat(this.toggleView, "\">\n\t\t\t\t\t\t\t\t<span class=\"toggleMonth\">{{month}}</span>\n\t\t\t\t\t\t\t\t<span class=\"toggleYear\">{{year}}</span>\n\t\t\t\t\t\t\t</strong>\n\t\t\t\t\t\t\t<span class=\"").concat(this.prefix, "-calendar-control next\"></span>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class=\"").concat(this.prefix, "-calendar-days\">{{days}}</div>\n\t\t\t\t\t<div class=\"").concat(this.prefix, "-calendar-grid viewMonth\">\n\t\t\t\t\t\t<div class=\"viewCalendar ").concat(this.prefix, "-calendar-grid__view-months\">{{months}}</div>\n\t\t\t\t\t\t<div class=\"viewCalendar ").concat(this.prefix, "-calendar-grid__view-month\">{{calendar-grid}}</div>\n\t\t\t\t\t\t<div class=\"viewCalendar ").concat(this.prefix, "-calendar-grid__view-years\">{{calendar-years}}</div>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class=\"").concat(this.prefix, "-calendar-actions\">\n\t\t\t\t\t\t\t<button cancel>").concat(this.cancelBtn, "</button>\n\t\t\t\t\t\t\t<button today>").concat(this.todayBtn, "</button>\n\t\t\t\t\t\t\t<button save>").concat(this.saveBtn, "</button>\n\t\t\t\t\t</div>\n\t\t\t</div>\n\t\t\t");
+      $template = $template.replace(/\{\{days\}\}/g, this.getDays()).replace(/\{\{month\}\}/g, this.months[this.getMonth() - 1]).replace(/\{\{year\}\}/g, this.getYear()).replace(/\{\{calendar-years\}\}/g, this.getYears()).replace(/\{\{calendar-grid\}\}/g, this.getCalendar()).replace('\t', '').replace('\n', '');
       this.el.innerHTML += $template;
       this.calendarGrid = this.el.querySelector(".".concat(this.prefix, "-calendar-grid"));
-      this.watchInput(id);
+      this.watchCalendar(parent, input);
+      this.watchToggleView(this.toggleView, parent);
+      this.watchClear(this.iconClear, parent, input);
+      this.watchToggle(id, parent);
+      this.watchInput(input, parent, this.iconClear);
+      this.updateInput(id);
+    }
+  }, {
+    key: "watchCalendar",
+    value: function watchCalendar(parent, input) {
+      var _this = this;
+
+      var $_days = parent.querySelectorAll(".".concat(this.prefix, "-calendar-grid__btn"));
+      var $_in = $_days.length;
+
+      for (var i = 0; i < $_in; i++) {
+        var el = $_days[i];
+        el.addEventListener('click', function (e) {
+          var $__btn = e.target;
+          var $__currentDay = $__btn.getAttribute('day');
+
+          _this.setDay($__currentDay);
+
+          _this.updateInput(input);
+
+          _this.removeClassSiblings($_days, $_in);
+
+          $__btn.classList.add('active');
+        }, false);
+      }
+    }
+  }, {
+    key: "watchToggleView",
+    value: function watchToggleView(view, parent) {
+      var _this2 = this;
+
+      var $__view = parent.querySelector("#".concat(view));
+      var $__viewContainer = parent.querySelector(".".concat(this.prefix, "-calendar-grid"));
+      $__view.addEventListener('click', function (e) {
+        var $_that = e.target;
+
+        if ($_that.classList.contains('toggleMonth')) {
+          _this2.removeClass(['viewMonths', 'viewYear'], $__viewContainer);
+
+          $__viewContainer.classList.add('viewMonths');
+        } else {
+          _this2.removeClass(['viewMonths', 'viewMonth'], $__viewContainer);
+
+          $__viewContainer.classList.add('viewYear');
+        }
+      }, false);
+    }
+  }, {
+    key: "removeClassSiblings",
+    value: function removeClassSiblings($siblings, $in) {
+      for (var $i = 0; $i < $in; $i++) {
+        var el = $siblings[$i];
+
+        if (el.classList.contains('active')) {
+          el.classList.remove('active');
+        }
+      }
+    }
+  }, {
+    key: "watchClear",
+    value: function watchClear(id, parent, input) {
+      var _this3 = this;
+
+      var $__clear = parent.querySelector("#".concat(id));
+      var $input = parent.querySelector("#".concat(input));
+      $__clear.addEventListener('click', function (e) {
+        _this3.setDay(1);
+
+        _this3.setMonth(1);
+
+        _this3.setYear(1);
+
+        _this3.updateInput(input);
+
+        e.target.classList.add('hide');
+      }, false);
+    }
+  }, {
+    key: "watchToggle",
+    value: function watchToggle(id, parent) {
+      var _this4 = this;
+
+      var $toggle = parent.querySelector("#".concat(id));
+      console.log($toggle);
+      $toggle.addEventListener('click', function (e) {
+        $toggle.nextElementSibling.classList.toggle("".concat(_this4.prefix, "-show"));
+      }, false); // $toggle.addEventListener('blur', e => {
+      //     $toggle.nextElementSibling.classList.remove(`${this.prefix}-show`);
+      // }, false);
     }
   }, {
     key: "watchInput",
-    value: function watchInput(id) {
-      var _this = this;
+    value: function watchInput(id, parent, clear) {
+      var _this5 = this;
 
-      var $input = document.querySelector("#".concat(id));
-      console.log($input);
-      $input.addEventListener('focus', function (e) {
-        $input.nextElementSibling.classList.add("".concat(_this.prefix, "-show"));
-      }, false);
-      $input.addEventListener('blur', function (e) {
-        $input.nextElementSibling.classList.remove("".concat(_this.prefix, "-show"));
-      }, false);
+      var $input = parent.querySelector("#".concat(id));
+      var $_keyUp = 0;
+      $input.addEventListener('keyup', function (e) {
+        var $__this = e.target;
+        var $__val = $__this.value;
+        var $__cont = $__val.length;
+        $_keyUp++;
+
+        if ($_keyUp == 1) {
+          parent.querySelector("#".concat(clear)).classList.remove('hide');
+        }
+
+        if ($__cont == 2 || $__cont == 5) {
+          $__this.value += '/';
+          $_keyUp++;
+        }
+
+        if ($_keyUp == 10) {
+          var $__newDate = $__this.value.split('/');
+
+          _this5.setDay($__newDate[0]);
+
+          _this5.setMonth($__newDate[1]);
+
+          _this5.setYear($__newDate[2]);
+
+          $_keyUp = 0;
+        }
+      }, false); // $toggle.addEventListener('blur', e => {
+      //     $toggle.nextElementSibling.classList.remove(`${this.prefix}-show`);
+      // }, false);
+    }
+  }, {
+    key: "updateInput",
+    value: function updateInput(id) {
+      var $_input = document.querySelector("#".concat(id));
+
+      if (this.getDay() == 1 && this.getMonth() == 1 && this.getYear() == 1) {
+        $_input.value = '';
+      } else {
+        var $_Day = this.getDay();
+        var $_Month = this.getMonth();
+        var $_Year = this.getYear();
+        $_input.value = "".concat($_Day < 10 ? '0' : '').concat($_Day, "/").concat($_Month < 10 ? '0' : '').concat($_Month, "/").concat($_Year);
+      }
     }
   }, {
     key: "getDays",
@@ -95,6 +232,20 @@ function () {
       for (var i = 0; i < $tdays; i++) {
         var e = this.days[i];
         $_template += "<span>".concat(e, "</span>");
+      }
+
+      return $_template;
+    }
+  }, {
+    key: "getYears",
+    value: function getYears() {
+      var $_template = '';
+      var $_current = this.getYear();
+      var $t = $_current - 50;
+      var $tLimit = $t + 100;
+
+      for (var i = $t; i < $tLimit; i++) {
+        $_template += "<span data-year=\"".concat(i, "\" ").concat($_current == i ? 'class="active"' : '', ">").concat(i, "</span>");
       }
 
       return $_template;
@@ -112,14 +263,13 @@ function () {
 
       for (var index = 1; index <= forMes; index++) {
         if (index == this.getDay()) {
-          $t += "<span ".concat(index == 1 ? 'style="grid-column-start:' + ($day + 1) + '"' : '', " class=\"").concat(this.prefix, "-calendar-grid--btn active\">").concat(this.print(index), "</span>");
+          $t += "<span day=\"".concat(index, "\" ").concat(index == 1 ? 'style="grid-column-start:' + ($day + 1) + '"' : '', " class=\"").concat(this.prefix, "-calendar-grid__btn active\">").concat(this.print(index), "</span>");
         } else {
-          $t += "<span ".concat(index == 1 ? 'style="grid-column-start:' + ($day + 1) + '"' : '', " class=\"").concat(this.prefix, "-calendar-grid--btn\">").concat(this.print(index), "</span>");
+          $t += "<span day=\"".concat(index, "\" ").concat(index == 1 ? 'style="grid-column-start:' + ($day + 1) + '"' : '', " class=\"").concat(this.prefix, "-calendar-grid__btn\">").concat(this.print(index), "</span>");
         }
       }
 
-      return $t; // buttons = grid.querySelectorAll('.calendar-grid-button');
-      // Calendar.watchCalendar(label, buttons, input, calendar);
+      return $t;
     }
   }, {
     key: "print",
@@ -169,6 +319,20 @@ function () {
         return 28;
       } else {
         return 30;
+      }
+    }
+  }, {
+    key: "removeClass",
+    value: function removeClass(classes, el) {
+      if (Array.isArray(classes)) {
+        var $_TC = classes.length;
+
+        for (var $i = 0; $i < $_TC; $i++) {
+          var e = classes[$i];
+          el.classList.remove(e);
+        }
+      } else {
+        el.classList.remove(classes);
       }
     }
   }, {
--- src/calendar.js
+++ src/calendar.js
@@ -1,7 +1,8 @@
 'use strict';
 class Calendar {
-    constructor({ el, language = 'es', name, prefix, date = null }) {
+    constructor({ el, language = 'es', name, prefix = 'mcp', date = null }) {
         this.el = document.querySelector(el);
+        this.that = el.split('#').join('');
         this.language = language;
         this.name = name;
         this.prefix = prefix;
@@ -10,7 +11,10 @@ class Calendar {
         this.initDate = date != null || date != undefined ? date.split('/') : null;
         this.calendarGrid = null;
 
-        this.inputID = `${el.split('#').join('')}-input`;
+        this.iconToggle = `${this.prefix}-${this.that}-dateNow`;
+        this.iconClear = `${this.prefix}-${this.that}-clear`;
+        this.inputId = `${this.prefix}-${this.that}-input`;
+        this.toggleView = `${this.prefix}-${this.that}-toggle-view`;
 
         if (this.initDate != null && Array.isArray(this.initDate)) {
             this.setDay(this.initDate[0]);
@@ -22,7 +26,7 @@ class Calendar {
             this.setYear(this.date.getFullYear());
         }
 
-        this.build(this.inputID);
+        this.build(this.iconToggle, this.inputId, this.el);
     }
 
     setLanguage(language) {
@@ -32,31 +36,47 @@ class Calendar {
                 this.months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
                 this.cancelBtn = 'cancel';
                 this.saveBtn = 'save';
+                this.todayBtn = 'today';
                 break;
             default:
                 this.days = ['d', 'l', 'm', 'mi', 'j', 'v', 's'];
                 this.months = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
                 this.cancelBtn = 'cancelar';
                 this.saveBtn = 'aceptar';
+                this.todayBtn = 'Hoy';
                 break;
         }
     }
 
-    build(id) {
+    build(id, input, parent) {
         this.setLanguage(this.language);
 
         let $template = `
-				<input type='text' class="${this.prefix}-input" id="${id}" placeholder='01/05/2019'/>
+				<span class="${this.prefix}-icon-clear hide" id="${this.iconClear}">
+					<span></span>
+				</span>
+				<input maxlength="10" type='text' class="${this.prefix}-input" id="${input}" placeholder='01/05/2019'/>
+				<span id="${this.iconToggle}" class="${this.prefix}-icon-get-date-now">
+					<span></span>
+				</span>
 				<div class="${this.prefix}-calendar">
 					<div class="${this.prefix}-calendar-header">
 							<span class="${this.prefix}-calendar-control previus"></span>
-							<strong>{{month}} {{year}}</strong>
+							<strong id="${this.toggleView}">
+								<span class="toggleMonth">{{month}}</span>
+								<span class="toggleYear">{{year}}</span>
+							</strong>
 							<span class="${this.prefix}-calendar-control next"></span>
 					</div>
 					<div class="${this.prefix}-calendar-days">{{days}}</div>
-					<div class="${this.prefix}-calendar-grid">{{calendar-grid}}</div>
+					<div class="${this.prefix}-calendar-grid viewMonth">
+						<div class="viewCalendar ${this.prefix}-calendar-grid__view-months">{{months}}</div>
+						<div class="viewCalendar ${this.prefix}-calendar-grid__view-month">{{calendar-grid}}</div>
+						<div class="viewCalendar ${this.prefix}-calendar-grid__view-years">{{calendar-years}}</div>
+					</div>
 					<div class="${this.prefix}-calendar-actions">
 							<button cancel>${this.cancelBtn}</button>
+							<button today>${this.todayBtn}</button>
 							<button save>${this.saveBtn}</button>
 					</div>
 			</div>
@@ -66,6 +86,7 @@ class Calendar {
         replace(/\{\{days\}\}/g, this.getDays()).
         replace(/\{\{month\}\}/g, this.months[this.getMonth() - 1]).
         replace(/\{\{year\}\}/g, this.getYear()).
+        replace(/\{\{calendar-years\}\}/g, this.getYears()).
         replace(/\{\{calendar-grid\}\}/g, this.getCalendar()).
         replace('\t', '').
         replace('\n', '');
@@ -73,19 +94,133 @@ class Calendar {
         this.el.innerHTML += $template;
         this.calendarGrid = this.el.querySelector(`.${this.prefix}-calendar-grid`);
 
-        this.watchInput(id);
+        this.watchCalendar(parent, input);
+        this.watchToggleView(this.toggleView, parent);
+        this.watchClear(this.iconClear, parent, input);
+        this.watchToggle(id, parent);
+        this.watchInput(input, parent, this.iconClear);
+        this.updateInput(id);
     }
 
-    watchInput(id) {
-        const $input = document.querySelector(`#${id}`);
-        console.log($input);
+    watchCalendar(parent, input) {
+        let $_days = parent.querySelectorAll(`.${this.prefix}-calendar-grid__btn`);
+        let $_in = $_days.length;
 
-        $input.addEventListener('focus', e => {
-            $input.nextElementSibling.classList.add(`${this.prefix}-show`);
+        for (let i = 0; i < $_in; i++) {
+            const el = $_days[i];
+
+            el.addEventListener('click', e => {
+                const $__btn = e.target;
+                const $__currentDay = $__btn.getAttribute('day');
+                this.setDay($__currentDay);
+                this.updateInput(input);
+
+                this.removeClassSiblings($_days, $_in);
+
+                $__btn.classList.add('active');
+            }, false);
+        }
+    }
+
+    watchToggleView(view, parent) {
+        const $__view = parent.querySelector(`#${view}`);
+        let $__viewContainer = parent.querySelector(`.${this.prefix}-calendar-grid`);
+
+        $__view.addEventListener('click', e => {
+            const $_that = e.target;
+
+
+            if ($_that.classList.contains('toggleMonth')) {
+                this.removeClass(['viewMonths', 'viewYear'], $__viewContainer);
+                $__viewContainer.classList.add('viewMonths');
+            } else {
+                this.removeClass(['viewMonths', 'viewMonth'], $__viewContainer);
+                $__viewContainer.classList.add('viewYear');
+            }
+
         }, false);
-        $input.addEventListener('blur', e => {
-            $input.nextElementSibling.classList.remove(`${this.prefix}-show`);
+    }
+
+    removeClassSiblings($siblings, $in) {
+        for (let $i = 0; $i < $in; $i++) {
+            let el = $siblings[$i];
+            if (el.classList.contains('active')) {
+                el.classList.remove('active');
+            }
+        }
+    }
+
+    watchClear(id, parent, input) {
+        const $__clear = parent.querySelector(`#${id}`);
+        const $input = parent.querySelector(`#${input}`);
+
+        $__clear.addEventListener('click', e => {
+            this.setDay(1);
+            this.setMonth(1);
+            this.setYear(1);
+
+            this.updateInput(input);
+            e.target.classList.add('hide');
         }, false);
+    }
+
+    watchToggle(id, parent) {
+        const $toggle = parent.querySelector(`#${id}`);
+        console.log($toggle);
+
+        $toggle.addEventListener('click', e => {
+            $toggle.nextElementSibling.classList.toggle(`${this.prefix}-show`);
+        }, false);
+        // $toggle.addEventListener('blur', e => {
+        //     $toggle.nextElementSibling.classList.remove(`${this.prefix}-show`);
+        // }, false);
+    }
+
+    watchInput(id, parent, clear) {
+        const $input = parent.querySelector(`#${id}`);
+        let $_keyUp = 0;
+        $input.addEventListener('keyup', e => {
+            let $__this = e.target;
+            let $__val = $__this.value;
+            let $__cont = $__val.length;
+
+            $_keyUp++;
+
+            if ($_keyUp == 1) {
+                parent.querySelector(`#${clear}`).classList.remove('hide');
+            }
+
+            if ($__cont == 2 || $__cont == 5) {
+                $__this.value += '/';
+                $_keyUp++;
+            }
+
+            if ($_keyUp == 10) {
+                let $__newDate = $__this.value.split('/');
+                this.setDay($__newDate[0]);
+                this.setMonth($__newDate[1]);
+                this.setYear($__newDate[2]);
+                $_keyUp = 0;
+            }
+        }, false);
+        // $toggle.addEventListener('blur', e => {
+        //     $toggle.nextElementSibling.classList.remove(`${this.prefix}-show`);
+        // }, false);
+    }
+
+    updateInput(id) {
+        const $_input = document.querySelector(`#${id}`);
+
+        if (this.getDay() == 1 && this.getMonth() == 1 && this.getYear() == 1) {
+            $_input.value = '';
+        } else {
+            let $_Day = this.getDay();
+            let $_Month = this.getMonth();
+            let $_Year = this.getYear();
+            $_input.value = `${$_Day < 10 ? '0' :''}${$_Day}/${$_Month < 10 ? '0' :''}${$_Month}/${$_Year}`;
+        }
+
+
     }
 
     getDays() {
@@ -96,6 +231,20 @@ class Calendar {
             const e = this.days[i];
 
             $_template += `<span>${e}</span>`;
+
+        }
+        return $_template;
+    }
+
+    getYears() {
+        let $_template = '';
+        let $_current = this.getYear();
+        let $t = $_current - 50;
+        let $tLimit = $t + 100;
+
+        for (let i = $t; i < $tLimit; i++) {
+
+            $_template += `<span data-year="${i}" ${$_current == i ? 'class="active"': ''}>${i}</span>`;
 
         }
         return $_template;
@@ -115,16 +264,13 @@ class Calendar {
 
         for (let index = 1; index <= forMes; index++) {
             if (index == this.getDay()) {
-                $t += `<span ${index == 1 ? 'style="grid-column-start:'+($day+1)+'"' : ''} class="${this.prefix}-calendar-grid--btn active">${this.print(index)}</span>`;
+                $t += `<span day="${index}" ${index == 1 ? 'style="grid-column-start:'+($day+1)+'"' : ''} class="${this.prefix}-calendar-grid__btn active">${this.print(index)}</span>`;
             } else {
-                $t += `<span ${index == 1 ? 'style="grid-column-start:'+($day+1)+'"' : ''} class="${this.prefix}-calendar-grid--btn">${this.print(index)}</span>`;
+                $t += `<span day="${index}" ${index == 1 ? 'style="grid-column-start:'+($day+1)+'"' : ''} class="${this.prefix}-calendar-grid__btn">${this.print(index)}</span>`;
             }
 
         }
         return $t;
-
-        // buttons = grid.querySelectorAll('.calendar-grid-button');
-        // Calendar.watchCalendar(label, buttons, input, calendar);
     }
 
     print(n) {
@@ -161,6 +307,20 @@ class Calendar {
             return 28;
         } else {
             return 30;
+        }
+    }
+
+    removeClass(classes, el) {
+        if (Array.isArray(classes)) {
+            let $_TC = classes.length;
+
+            for (let $i = 0; $i < $_TC; $i++) {
+                const e = classes[$i];
+
+                el.classList.remove(e);
+            }
+        } else {
+            el.classList.remove(classes);
         }
     }
 
